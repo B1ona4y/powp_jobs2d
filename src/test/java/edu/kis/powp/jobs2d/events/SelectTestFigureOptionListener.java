de678c713fa81be3e8d7c35df8e3ade0a3c3f399
B1ona4y <eu.oleynich@gmail.com>
2026-03-12 21:58:11 +0100

    Modified SelectTestFigureOptionListener to enable actionPerformed to operate on many figures using Consumer

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,20 +2,24 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.function.Consumer;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
-import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+	private final DriverManager driverManager;
+	private final Consumer<Job2dDriver> drawingAction;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, Consumer<Job2dDriver> drawingAction) {
 		this.driverManager = driverManager;
-	}
+        this.drawingAction = drawingAction;
+    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		Job2dDriver driver = driverManager.getCurrentDriver();
+		drawingAction.accept(driver);
 	}
 }
