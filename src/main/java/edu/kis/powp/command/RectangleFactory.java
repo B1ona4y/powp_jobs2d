ec20f2ea0ab53fe9c77433f205f90ddc1c1a2af2
YevhenOleinichenko <eu.oleynich@gmail.com>
2026-03-26 11:17:06 +0100

    fix: RectangleFactory

--- src/main/java/edu/kis/powp/command/RectangleFactory.java
+++ src/main/java/edu/kis/powp/command/RectangleFactory.java
@@ -6,10 +6,10 @@ public class RectangleFactory {
     public static ComplexCommand rectangleCommand(Job2dDriver driver, int x, int y, int width, int length){
         ComplexCommand command = new ComplexCommand();
         command.addCommand(new SetPositionCommand(driver, x, y));
-        command.addCommand(new SetPositionCommand(driver, x + length, y));
-        command.addCommand(new SetPositionCommand(driver, x + length, y + width));
-        command.addCommand(new SetPositionCommand(driver, x, y + width));
-        command.addCommand(new SetPositionCommand(driver, x, y));
+        command.addCommand(new OperateToCommand(driver, x + length, y));
+        command.addCommand(new OperateToCommand(driver, x + length, y + width));
+        command.addCommand(new OperateToCommand(driver, x, y + width));
+        command.addCommand(new OperateToCommand(driver, x, y));
         return command;
     }
 }
