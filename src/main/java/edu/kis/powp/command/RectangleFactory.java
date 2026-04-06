119605d3110ee3cc5021b512c8370f4f2a887537
B1ona4y <eu.oleynich@gmail.com>
2026-04-06 11:20:02 +0200

    complex command - removed code repetition

--- src/main/java/edu/kis/powp/command/RectangleFactory.java
+++ src/main/java/edu/kis/powp/command/RectangleFactory.java
@@ -3,12 +3,15 @@ package edu.kis.powp.command;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class RectangleFactory {
-    public static ComplexCommand rectangleCommand(Job2dDriver driver, int x, int y, int width, int length){
+    public static ComplexCommand rectangleCommand(Job2dDriver driver, int x, int y, int width, int length) {
+        int right = x + length;
+        int bottom = y + width;
+
         ComplexCommand command = new ComplexCommand();
         command.addCommand(new SetPositionCommand(driver, x, y));
-        command.addCommand(new OperateToCommand(driver, x + length, y));
-        command.addCommand(new OperateToCommand(driver, x + length, y + width));
-        command.addCommand(new OperateToCommand(driver, x, y + width));
+        command.addCommand(new OperateToCommand(driver, right, y));
+        command.addCommand(new OperateToCommand(driver, right, bottom));
+        command.addCommand(new OperateToCommand(driver, x, bottom));
         command.addCommand(new OperateToCommand(driver, x, y));
         return command;
     }
